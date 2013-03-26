09397b9358146d3f7c1ea6f294f587db36d072dd
roszman <roszman@gmail.com>
2013-03-26 21:54:40 +0100

    EF setup, ninject and mock, list product page

---
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using Ninject;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectControllerFactory : DefaultControllerFactory
+    {
+// ReSharper disable FieldCanBeMadeReadOnly.Local
+        private IKernel _ninjectKernel;
+// ReSharper restore FieldCanBeMadeReadOnly.Local
+
+        public NinjectControllerFactory()
+        {
+            _ninjectKernel = new StandardKernel();
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            //Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //    {
+            //        new Product {Name = "Football", Price = 25},
+            //        new Product {Name = "Surf board", Price = 25},
+            //        new Product {Name = "Running shoes", Price = 25},
+
+            //    }.AsQueryable());
+
+            _ninjectKernel.Bind<IProductRepository>().To<EFProductRepository>();
+        }
+
+        protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+        {
+            return controllerType == null ? null : (IController) _ninjectKernel.Get(controllerType);
+        }
+    }
+}
