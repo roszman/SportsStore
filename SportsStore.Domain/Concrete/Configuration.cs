42f634fccef75cecc3d7d9b1e5936ae836d30b03
roszman <roszman@gmail.com>
2013-05-12 18:29:59 +0200

    EF Configuration class (not added previously)

---
+++ SportsStore.Domain/Concrete/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class Configuration : DbMigrationsConfiguration<EFDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+    }
+}
--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
+        public int TestProperty { get; set; }
     }
 }
